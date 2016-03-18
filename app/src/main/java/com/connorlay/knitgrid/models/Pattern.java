31aeaa35d0899664f116423d00bd140c80211630
Connor Lay <connor.lay@me.com>
2016-03-17 23:20:07 -0700

    Add Pattern and StitchPatternRelation models to persist patterns in the database.

---
+++ app/src/main/java/com/connorlay/knitgrid/models/Pattern.java
@@ -0,0 +1,101 @@
+package com.connorlay.knitgrid.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.orm.SugarRecord;
+
+import java.util.List;
+
+/**
+ * Created by connorlay on 3/17/16.
+ */
+public class Pattern extends SugarRecord implements Parcelable {
+    private String name;
+    private int rows;
+    private int cols;
+    private boolean alternating;
+
+    public Pattern() {
+
+    }
+
+    public Pattern(String name, int rows, int columns, boolean alternating) {
+        this.name = name;
+        this.rows = rows;
+        this.cols = columns;
+        this.alternating = alternating;
+    }
+
+    protected Pattern(Parcel in) {
+        name = in.readString();
+        rows = in.readInt();
+        cols = in.readInt();
+        alternating = in.readByte() != 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeInt(rows);
+        dest.writeInt(cols);
+        dest.writeByte((byte) (alternating ? 1 : 0));
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Pattern> CREATOR = new Creator<Pattern>() {
+        @Override
+        public Pattern createFromParcel(Parcel in) {
+            return new Pattern(in);
+        }
+
+        @Override
+        public Pattern[] newArray(int size) {
+            return new Pattern[size];
+        }
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return cols;
+    }
+
+    public boolean isAlternating() {
+        return alternating;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public void setColumns(int columns) {
+        this.cols = columns;
+    }
+
+    public void setAlternating(boolean alternating) {
+        this.alternating = alternating;
+    }
+
+    public List<StitchPatternRelation> getStitchRelations() {
+        return StitchPatternRelation.find(StitchPatternRelation.class, "pattern = ?", String.valueOf(this.getId()));
+    }
+
+    public void setStitch(Stitch stitch, int row, int column) {
+        new StitchPatternRelation(this, stitch, row, column).save();
+    }
+}
---
+++ app/src/main/java/com/connorlay/knitgrid/models/StitchPatternRelation.java
@@ -0,0 +1,90 @@
+package com.connorlay.knitgrid.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by connorlay on 3/17/16.
+ */
+public class StitchPatternRelation extends SugarRecord implements Parcelable {
+    private Pattern pattern;
+    private Stitch stitch;
+    private int row, col;
+
+    public StitchPatternRelation() {
+
+    }
+
+    public StitchPatternRelation(Pattern pattern, Stitch stitch, int row, int column) {
+        this.pattern = pattern;
+        this.stitch = stitch;
+        this.row = row;
+        this.col = column;
+    }
+
+    protected StitchPatternRelation(Parcel in) {
+        this.pattern = in.readParcelable(Pattern.class.getClassLoader());
+        this.stitch = in.readParcelable(Stitch.class.getClassLoader());
+        this.row = in.readInt();
+        this.col = in.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(pattern, flags);
+        dest.writeParcelable(stitch, flags);
+        dest.writeInt(row);
+        dest.writeInt(col);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<StitchPatternRelation> CREATOR = new Creator<StitchPatternRelation>() {
+        @Override
+        public StitchPatternRelation createFromParcel(Parcel in) {
+            return new StitchPatternRelation(in);
+        }
+
+        @Override
+        public StitchPatternRelation[] newArray(int size) {
+            return new StitchPatternRelation[size];
+        }
+    };
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public Stitch getStitch() {
+        return stitch;
+    }
+
+    public void setStitch(Stitch stitch) {
+        this.stitch = stitch;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+}
